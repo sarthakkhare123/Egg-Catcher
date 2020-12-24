9e60a1f172a821486de31a134b30abd9b305ea3f
Sarthak Khare <46642529+sarthakkhare123@users.noreply.github.com>
2020-12-24 22:17:32 +0530

    Update Egg Catcher.py

--- Egg Catcher.py
+++ Egg Catcher.py
@@ -1,40 +1,55 @@
 from itertools import cycle
 from random import randrange
 from tkinter import Tk , Canvas , messagebox , font
+from PIL import ImageTk,Image
+import pygame
 
 # setting the width and the height of the background
-canvas_width = 800
-canvas_height = 400
+canvas_width = 1352
+canvas_height = 794
 
 win = Tk()
+
 # for backround
 c = Canvas(win , width = canvas_width ,  height = canvas_height , background = 'deep sky blue')
+
+# Main background image
+img  = ImageTk.PhotoImage(Image.open('maingameforest.png'))
+c.create_image(700,320,image=img)
+
 # falling Eggs background
 c.create_rectangle(-5, canvas_height - 100 , canvas_width + 5 , canvas_height + 5 , fill='sea green', width=0)
+
 # Score box
 c.create_oval(-80,-80,120,120,fill='orange' , width=0)
 c.pack()
 
+
 # list for the random colors of the eggs
 color_cycle = cycle(['light blue' , 'light pink' , 'light yellow','light green' , 'red', 'blue' , 'green','black'])
+
 # setting the width,height of the eggs
 egg_width = 45
 egg_height = 55
+
 # increament score on catching
 egg_score = 10
+
 # speed of eggs
 egg_speed = 500
-# Appering in the interval of 4ms
+
+# Appearing in the interval of 4ms
 egg_interval = 4000
+
 # Difficulty_factor will increase the hardness of the game
 difficulty_factor = 0.95
 
-# setting catcher color,width,heightm and position
+# setting catcher color,width,height and position
 catcher_color = 'blue'
 catcher_width = 100
 catcher_height = 100
 catcher_start_x = canvas_width / 2 - catcher_width / 2
-catcher_start_y = canvas_height -catcher_height - 20
+catcher_start_y = canvas_height -catcher_height - 100
 catcher_start_x2 = catcher_start_x + catcher_width
 catcher_start_y2 = catcher_start_y + catcher_height
 
@@ -42,11 +57,12 @@ catcher = c.create_arc(catcher_start_x ,catcher_start_y ,catcher_start_x2,catche
 
 score = 0
 score_text = c.create_text(10,10,anchor='nw' , font=('Arial',18,'bold'),fill='darkblue',text='Score : ' + str(score))
-#  For the number of lives
+
 lives_remaning = 3
 lives_text = c.create_text(canvas_width-10,10,anchor='ne' , font=('Arial',18,'bold'),fill='darkblue',text='Lives : ' + str(lives_remaning))
 
 eggs = []
+
 
 # Creation of eggs
 def create_eggs():
@@ -55,8 +71,7 @@ def create_eggs():
     new_egg = c.create_oval(x,y,x+egg_width,y+egg_height,fill=next(color_cycle),width=0)
     eggs.append(new_egg)
     win.after(egg_interval,create_eggs)
-    
-    
+
 # Movement of eggs
 def move_eggs():
     for egg in eggs:
@@ -66,6 +81,11 @@ def move_eggs():
             egg_dropped(egg)
     win.after(egg_speed,move_eggs)
 
+## GAME SOUND ##
+pygame.init()
+pygame.mixer.music.load('gameplay_sound.mp3')
+pygame.mixer.music.play()
+
 #  when egg will drop
 def egg_dropped(egg):
     eggs.remove(egg)
@@ -73,12 +93,15 @@ def egg_dropped(egg):
     lose_a_life()
     if lives_remaning == 0:
         messagebox.showinfo('GAME OVER!' , 'Final Score : ' + str(score))
-        win.destroy()
+        pygame.mixer.music.stop()
+        quit()
+
 # On droping of egg the life will be decrement by 1
 def lose_a_life():
     global lives_remaning
     lives_remaning -= 1
     c.itemconfigure(lives_text , text='Lives : ' + str(lives_remaning))
+
 # Check if there is a catch or miss by the catcher
 def catch_check():
     (catcher_x,catcher_y,catcher_x2,catcher_y2) = c.coords(catcher)
@@ -89,6 +112,7 @@ def catch_check():
             c.delete(egg)
             increase_score(egg_score)
     win.after(100,catch_check)
+
 # Increase the score on catch
 def increase_score(points):
     global score , egg_speed , egg_interval
@@ -96,16 +120,20 @@ def increase_score(points):
     egg_speed = int(egg_speed * difficulty_factor)
     egg_interval = int(egg_interval * difficulty_factor)
     c.itemconfigure(score_text , text='Score : ' + str(score))
+
 # Movement for left
 def move_left(event):
     (x1,y1,x2,y2) = c.coords(catcher)
     if x1 > 0:
         c.move(catcher,-20,0)
+
+        
 # Movement for right
 def move_right(event):
     (x1,y1,x2,y2) = c.coords(catcher)
     if x2 < canvas_width:
         c.move(catcher,20,0)
+
 # Binding the left and right
 c.bind('<Left>' , move_left)
 c.bind('<Right>' , move_right)
@@ -114,5 +142,4 @@ c.focus_set()
 win.after(1000,create_eggs)
 win.after(1000,move_eggs)
 win.after(1000,catch_check)
-
 win.mainloop()
